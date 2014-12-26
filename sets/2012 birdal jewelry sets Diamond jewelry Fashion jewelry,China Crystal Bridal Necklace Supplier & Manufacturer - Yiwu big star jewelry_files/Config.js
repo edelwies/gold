fe17af59ec54ebdd132e51cb25428e3f086840b0
Mary Salehi <mary_alife@yahoo.com>
2014-12-26 12:03:01 +0330

    logo movie & some files about  the images added

---
+++ sets/2012 birdal jewelry sets Diamond jewelry Fashion jewelry,China Crystal Bridal Necklace Supplier & Manufacturer - Yiwu big star jewelry_files/Config.js
@@ -0,0 +1,54 @@
+﻿//主面板底部工具条区域
+var Fps_Conf_customMainButtomBarPanel = {
+        content: ' ',
+        height: 20,
+        width: 136
+};
+
+
+//软件名称
+var Fps_Conf_softName='Service Online';
+//用户列表名称
+var Fps_Conf_listServiceName="Service Online";
+
+//短信开关
+Fps_Conf_sendSMSEnable = '0';
+//设置颜色的参数
+Fps_Conf_colorType =4
+
+//聊天窗口大小
+Fps_Conf_chatSize = {
+   width : 501,
+   height : 323
+};
+
+
+//自定义主界面显示位置
+var Fps_Conf_customMainPostion = {
+   //水平 0 左 1 中 2 右
+   hPosition: 2,
+   //垂直 0 上 1 中 2 下
+   vPosition: 1
+};
+
+
+var Fps_Conf_welcomeGreetings = '欢迎访问我们的网站,竭诚为您服务!';
+var Fps_Conf_welcomeGreetings_eng="Welcome to visit website, at your dedicated service!";
+//托盘按钮图片 点击关闭后显示的按钮
+/*var Fps_Conf_customTrayButton = {
+       LeftButtonImage: ' http://im.tradevv.com:8080/cimls/webtm/templets/ECKJ/image/TrayButtonLeft.gif',
+       RightButtonImage: ' http://im.tradevv.com:8080/cimls/webtm/templets/ECKJ/image/TrayButtonRight.gif',
+       ButtonWidth: 37,
+       ButtonHeight: 131
+};*/
+
+
+//主面板顶部自定义区域
+var Fps_Conf_customMainTopPanel = {
+       content: '<img src=" http://im.tradevv.com:8080/cimls/webtm/templets/2011/image/TopPanel.gif" height="26"/>',
+       height: 26,
+       width: 135
+};
+
+//软件标志图片
+/*var Fps_Conf_customSoftICON = ' http://im.tradevv.com:8080/cimls/webtm/templets/2011/image/ICON.gif';*/
